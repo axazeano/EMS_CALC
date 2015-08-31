ef36dc377c58e9b46ec329897cf58ccd096b37d5
Axazeano <vovchik161@gmail.com>
2015-08-31 22:53:29 +0400

    Rebuild GUI

--- GUI.py
+++ GUI.py
@@ -14,28 +14,56 @@ root = Tkinter.Tk()
 root.wm_title('EMS Calc')
 
 label_from = ttk.Label(root, text='From location:')
-label_from.grid(row=0, column=0)
+label_from.grid(row=0, column=0, sticky='e')
+
 entry_from = ttk.Entry(root)
-entry_from.grid(row=0, column=1)
+entry_from.grid(row=0, column=2)
 
 label_to = ttk.Label(root, text='To location:')
-label_to.grid(row=1, column=0)
-entry_to = ttk.Entry(root)
-entry_to.grid(row=1, column=1)
+label_to.grid(row=1, column=0, sticky='e')
+
+combobox_from = ttk.Combobox(root, values=[u"..."], height=10, width=30)
+combobox_from.grid(row=0, column=2)
+
+combobox_to = ttk.Combobox(root, values=[u"..."], height=10, width=30)
+combobox_to.grid(row=1, column=2)
 
 type_of_package_var = Tkinter.IntVar()
 
 label_type = ttk.Label(root, text='Type of package:')
-label_type.grid(row=2, column=0)
+label_type.grid(row=2, column=0, sticky='e',)
 
 radiobutton_doc = ttk.Radiobutton(root, variable=type_of_package_var, value=1, text='Documents')
-radiobutton_doc.grid(row=2, column=1)
+radiobutton_doc.grid(row=2, column=2, sticky='w')
 
 radiobutton_attr = ttk.Radiobutton(root, variable=type_of_package_var, value=2, text='Commodity Investments')
-radiobutton_attr.grid(row=2, column=2)
+radiobutton_attr.grid(row=3, column=2, sticky='w')
+
+label_weight = ttk.Label(root, text='Weight:')
+label_weight.grid(row=4, column=0, sticky='e')
+
+entry_weight = ttk.Entry(root)
+entry_weight.grid(row=4, column=2, sticky='w')
+
+label_cost = ttk.Label(root, text='Cost of delivery:')
+label_cost.grid(row=5, column=0, sticky='e')
+label_cost['state'] = 'disabled'
+
+label_cost_of_delivery = ttk.Label(root, text='')
+label_cost_of_delivery.grid(row=5, column=2, sticky='w')
+
+label_duration = ttk.Label(root, text='Duration of delivery:')
+label_duration.grid(row=6, column=0, sticky='e')
+label_duration['state'] = 'disabled'
+
+label_duration_of_delivery = ttk.Label(root, text='')
+label_duration_of_delivery.grid(row=6, column=2, sticky='w')
+
+button_calculate = ttk.Button(root, text='Calculate')
+button_calculate.grid(row=7, column=2)
 
 label_status = ttk.Label(root, text='')
-label_status.grid(row=3, column=0)
+label_status.grid(row=8, column=0)
 # =========END GUI DESCRIPTION==========
 
 
